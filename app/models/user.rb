66a42409709627dafb41abcb93bd0ba1a1660b24
adrien66 <adrien66.pub@gmail.com>
2019-08-13 12:44:05 +0200

    welcome

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
