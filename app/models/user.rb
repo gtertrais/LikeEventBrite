22c144d676a9515a00440dae816948a7beee22bd
adrien66 <adrien66.pub@gmail.com>
2019-08-13 11:22:55 +0200

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ApplicationRecord
-  after_create :welcome_send
-  has_many :event
-  has_many :attendances
-  has_many :events, through: :attendances
-
-  def welcome_send
-    UserMailer.welcome_email(self).deliver_now
-  end
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
