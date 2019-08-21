4e42e99b03c480548318813b97c04b2dbd04afb1
Gaspard49 <gaspard.tertrais@gmail.com>
2019-08-21 19:04:20 +0200

    add bootstrap index show

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,12 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :attendances
   has_many :users, through: :attendances
-  has_many_attached :photos
+  has_one_attached :photos
 
   validates :start_date, :duration, :title, :description, :price, :location, presence: true
 
-  validates :start_date_cannot_be_in_the_past, presence: true
+  validates :photos, presence: true
+ # validates :start_date_cannot_be_in_the_past, presence: true
   validates :duration, numericality: { greater_than: 0 }
   validates :title, length: { in: 5..140 }
   validates :description, length: { in: 20..1000 }
