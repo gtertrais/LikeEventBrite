f9949c4721bf2c09d04ec785952cdddffb627e8f
Gaspard49 <gaspard.tertrais@gmail.com>
2019-08-31 16:25:59 +0200

    merge

---
+++ db/migrate/20190815163125_add_first_name_to_users.rb
@@ -0,0 +1,8 @@
+class AddFirstNameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :date_of_birth, :datetime
+    add_column :users, :description, :text
+  end
+end
